91930a2a87f2410e9ec1f7fda7d1d5e6882892eb
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2020-03-23 12:19:12 -0300

    voltado para filtro antigo

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -12,7 +12,7 @@ import * as xml2js from 'xml2js';
 export class FeedComponent implements OnInit {
 
   RssData: NewsRss;
-  filtro = ['saude', 'corona', 'ibuprofeno', 'covid19', 'covid-19', 'pandemia', 'Corona Vírus', 'Covid-19', 'Vírus', 'Corona',  'quarentena', 'Coronavírus', 'coronavírus'];
+  filtro: string[] = ['saude', 'corona', 'covid19', 'covid-19', 'pandemia', 'Corona Vírus', 'Covid-19', 'Vírus', 'Corona',  'quarentena', 'Coronavírus', 'coronavírus'];
   news: string[] = [];
 
   constructor(private globals: Globals, private http: HttpClient) { }
@@ -22,8 +22,6 @@ export class FeedComponent implements OnInit {
     this.news.map(async url => {
       await this.getFeed(url);
      });
-    // await this.getFeed('https://noticiasdeararas.com.br/feed');
-    // await this.getFeed('https://reporterbetoribeiro.com.br/feed/');
   }
 
   async getFeed(url: string) {
@@ -39,64 +37,21 @@ export class FeedComponent implements OnInit {
         const parseString = xml2js.parseString;
         parseString(data, (err, result: NewsRss) => {
           this.RssData = result;
-          this.RssData.rss.channel[0].item.forEach(el => {
+          this.RssData.rss.channel[0].item.map(el => {
             const title = this.removeAcento(el.title[0]);
             const description = this.removeAcento(el.description[0]);
-            const source = el.guid;
-
-            this.filtro.map(filtro => {
-              if (title.includes(filtro) && description.includes(filtro)){
+            
+            for (let i = 0; i < this.filtro.length; i++) {
+              if (title.includes(this.filtro[i]) || description.includes(this.filtro[i])) {
                 el.description[0] = el.description[0].replace(/<\/?[^>]+(>|$)/g, '');
-                this.globals.itens['push'](el);
+                this.globals.itens.push(el);
+                break;
               }
-            })
-            // for (let i = 0; i < this.filtro.length; i++) {
-            //   if (title.includes(this.filtro[i]) || description.includes(this.filtro[i])) {
-            //     console.log(el)
-            //     el.description[0] = el.description[0].replace(/<\/?[^>]+(>|$)/g, '');
-            //     this.globals.itens['push'](el);
-            //     break;
-            //   }
-            // }
+            }
           });
-          // console.log(this.globals.itens);
         });
 
       });
-
-
-    // const requestOptions: Object = {
-    //   observe: 'body',
-    //   responseType: 'text'
-    // };
-    // this.http
-    //   .post<any>('http://localhost/rss.php',
-    //   {
-    //     url: 'https://reporterbetoribeiro.com.br/feed/'
-    //   },
-    //   requestOptions)
-    //   .subscribe(data => {
-    //     console.log(data);
-    //     const parseString = xml2js.parseString;
-    //     parseString(data, (err, result: NewsRss) => {
-    //       this.RssData = result;
-    //       console.log(this.RssData);
-    //     });
-    //   });
-
-    // const parser = new Parser();
-    // const feed = await parser.parseURL('https://www.reddit.com/.rss');
-    // console.log(feed);
-    // this.service.getFeed('https://reporterbetoribeiro.com.br/feed/')
-    // .subscribe(noticias => {
-    //   console.log(noticias);
-    //   // if (noticias.length <= 0) { this.globals.finished = true; }
-
-    //   // noticias.forEach(element => {
-    //   //   // element.hora_abertura = element.data_abertura.substring(11, 16);
-    //   //   this.globals.itens['push'](element);
-    //   // });
-    // });
   }
 
   removeAcento (text: string) {
