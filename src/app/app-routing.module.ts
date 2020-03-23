ea3cc8302052feb4c59ee95fd2c8779ec13a3616
DESKTOP-4G0D9IT\André-PC <andre.fantinato@gmail.com>
2020-03-23 18:03:20 -0300

    layout menu, páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+import { LinksComponent } from './links/links.component';
+import { ContributorsComponent } from './contributors/contributors.component';
+import { ContactComponent } from './contact/contact.component';
+import { FeedComponent } from './feed/feed.component';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+const appRoutes: Routes = [
+  { path: '', component: FeedComponent },
+  { path: 'noticias', redirectTo: '', pathMatch: 'full' },
+  { path: 'contato', component: ContactComponent, pathMatch: 'full' },
+  { path: 'links', component: LinksComponent, pathMatch: 'full' },
+  { path: 'colaboradores', component: ContributorsComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '' }
+];
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(appRoutes) ]
 })
 export class AppRoutingModule { }
