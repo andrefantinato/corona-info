3708185bb87f8b6348ce4c49dd34bcdb577d5257
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2020-03-24 14:25:02 -0300

    Menu responsivo add

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  collapsed = false;
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+    console.log(this.collapsed);
   }
 
 }
